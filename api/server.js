9e3a1f28a140ed5c54bad16ee31a7c884846ef25
Eian Carter <eian@bricleir.com>
2020-02-18 17:00:55 -0800

    added missing db to server js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const db = require('../data/configDB');
 
 const carsRouter = require('../cars/carsRouter.js');
 
@@ -9,5 +10,9 @@ server.use(helmet());
 server.use(express.json());
 
 server.use('/api/cars', carsRouter);
+server.get('/', (req, res) => {
+    res.send('<h3>hello world</h3>');
+});
+
 
 module.exports = server;
