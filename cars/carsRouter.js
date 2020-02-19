9ff280879bc57c9ec589f03de50ec656951c7d1d
Eian Carter <eian@bricleir.com>
2020-02-19 09:28:20 -0800

    fixed put and delete

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -31,7 +31,7 @@ router.post("/", (req, res) => {
   db("cars")
     .insert(carData)
     .then(ids => {
-      db("fruits")
+      db("cars")
         .where({ id: ids[0] })
         .then(newCarEntry => {
           res.status(201).json(newCarEntry);
@@ -40,6 +40,41 @@ router.post("/", (req, res) => {
     .catch(err => {
       console.log("POST error", err);
       res.status(500).json({ message: "Failed to store data" });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+  db("cars")
+    .where({ id })
+    .update(changes)
+    .then(car => {
+      res.json(car);
+    })
+    .catch(err => {
+      console.log("UPDATE error", err)
+      res.status(500).json({ message: "Failed to retrieve car" });
+    });
+});
+
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("cars")
+    .where({ id })
+    .del()
+    .then(count => {
+      if(count > 0) {
+          res.status(200).json({ message: "the candidate has been deleted" });
+      } else {
+          res.status(404).json({ message: "candidate cannot be found" });
+      }
+    })
+    .catch(err => {
+      console.log("DELETE error", err)
+      res.status(500).json({ message: "Failed to delete car" });
     });
 });
 
